6997cd4a681da1f2e48ad3245b171bba76a20bd7
DouglasAmorim <amorimdos@gmail.com>
2021-08-26 22:26:09 -0300

    midia session

--- client.js
+++ client.js
@@ -1,15 +1,156 @@
-
-const io = require('socket.io')
 const socket = io()
+const audio = document.querySelector("video");
+
+let roomId = 1
+let name
+let isCreator
+let idSocket
+let numberOfClients
+
+let loginDetails = {roomId, name, isCreator, idSocket, numberOfClients}
+
+alert('Informe o nome do usuario');
+
+connectButton.addEventListener('click', () => { joinRoom(userInput.value) })
+
+const peerConnections = {};
+var midia;
+
+const iceServers = {
+  iceServers: [
+  { urls: "stun:smu20211.sj.ifsc.edu.br" },
+      {
+        urls: "turn:smu20211.sj.ifsc.edu.br",
+        username: "douglas.as1997",
+        credential: "smu20211"
+      }
+  ],
+}
 
 socket.on("connect", function() {
-
-    // Enviando mensagem inicial de conexão com o servidor
-    socket.emit('JOIN', { message: '' })
 });
 
-socket.on('ROOM_JOINED', function(data){
+socket.on('ROOM_CREATED', (detailsReceived, playersReceived) => {
+  navigator.mediaDevices
+    .getUserMedia({ video: false, audio: true })
+    .then((stream) => {
+      midia = stream;
+    })
+    .catch((error) => console.log(error));
 
-    console.log("Conectado ao Servidor, nosso ID é %s", socket.id);
-    console.log(data);
+  console.log('Socket event callback: ROOM_CREATED')
+  console.log(`Connection Details received from Server: ${detailsReceived}`);
+  console.log(`Actual players on room received from Server: ${playersReceived}`);
+
+  loginDetails = detailsReceived
+
+  userInput.disabled = true
+  connectButton.disabled = true
+
+  loginDetails.isRoomCreator = true
+  loginDetails.numberOfClients = playersReceived.lenght
+})
+
+socket.on('ROOM_JOINED',  (detailsReceived, playersReceived) => {
+  console.log('Socket event callback: ROOM_JOINED')
+  console.log("Conectado ao Servidor, nosso ID é %s", socket.id);
+  console.log(`Connection Details received from Server: ${detailsReceived}`);
+  console.log(`Actual players on room received from Server: ${playersReceived}`);
+  
+  loginDetails = detailsReceived
+
+  userInput.disabled = true
+  connectButton.disabled = true
+
+  loginDetails.isRoomCreator = false
+  loginDetails.numberOfClients = playersReceived.lenght
+
+  
+  navigator.mediaDevices
+    .getUserMedia({ video: false, audio: true })
+    .then((stream) => {
+      midia = stream;
+      const peerConnection = new RTCPeerConnection(iceServers);
+      peerConnections[playersReceived[0]] = peerConnection;
+
+      stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
+      peerConnection.onicecandidate = event => {
+        if (event.candidate) {
+          console.log("SENDING ICE_CANDIDATE")
+          socket.emit("ICE_CANDIDATE", playersReceived[0], event.candidate)
+        }
+      };
+
+      peerConnection.ontrack = ({ streams: [midia] }) => {
+        audio.srcObject = midia;
+      };
+
+      peerConnection
+      .createOffer()
+      .then(sdp => peerConnection.setLocalDescription(sdp))
+      .then(() => {
+        console.log("SENDING OFFER")
+        socket.emit("OFFER", playersReceived[0], peerConnection.localDescription);
+      });
+    });
 });
+
+socket.on("OFFER", (id, description) => {
+  peerConnection = new RTCPeerConnection(iceServers);
+
+  midia
+    .getTracks()
+    .forEach((track) => peerConnection.addTrack(track, midia));
+
+  peerConnection
+    .setRemoteDescription(description)
+    .then(() => peerConnection.createAnswer())
+    .then(sdp => peerConnection.setLocalDescription(sdp))
+    .then(() => {
+      console.log("SENDING ANSWER")
+      socket.emit("ANSWER", id, peerConnection.localDescription);
+    });
+  peerConnection.ontrack = ({ streams: [midia] }) => {
+    audio.srcObject = midia;
+  };
+  peerConnection.onicecandidate = event => {
+    if (event.candidate) {
+      console.log("SENDING ICE_CANDIDATE")
+      socket.emit("ICE_CANDIDATE", id, event.candidate);
+    }
+  };
+});
+
+socket.on("ANSWER", (id, description) => {
+  console.log("RECEIVED ANSWER")
+  peerConnections[id].setRemoteDescription(description);
+  console.log(peerConnections[id]);
+});
+
+socket.on("ICE_CANDIDATE", (id, candidate) => {
+  console.log("RECEIVED ICE_CANDIDATE")
+  peerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
+  console.log(peerConnections[id]);
+});
+
+socket.on("DISCONNECT", id => {
+  peerConnections[id].close();
+  delete peerConnections[id];
+  console.log(peerConnections[id]);
+});
+
+window.onunload = window.onbeforeunload = () => {
+  socket.close();
+};
+
+function joinRoom(user) {
+    if (user === '') {
+      alert('Informe o nome do usuario');
+    } else {
+      loginDetails.name = user;
+      loginDetails.roomId = 1;
+
+      console.log('Enviou JOIN com socket id: ', socket.id) 
+      socket.emit('JOIN', loginDetails)
+    }
+  }
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
-const { ClientRequest } = require('http')
-const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const express = require('express');
+const { ClientRequest } = require('http');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 
 // cria uma rota para o arquivo index.html
 app.get('/', function(req, res){
@@ -10,27 +10,287 @@ app.get('/', function(req, res){
 });
 
 // Lista de clientes
-let clients = [];
+var players = [];
 
 // Escutando Eventos
 io.sockets.on('connection', function(socket) {
 
     // Recebimento JOIN
-    socket.on('JOIN', function(data) {
+    socket.on('JOIN', (loginDetails) =>  {
+        console.log(`Received Message 'JOIN' from user:  ${loginDetails.name}`);
+        console.log(players)
+        if (players[0] == null) {
+          console.log(`Criando sala ${loginDetails.roomId}, emitido 'ROOM_CREATED to user' ${loginDetails.name}`)
+          
+          players[0] = socket.id;
+          loginDetails.isCreator = true 
+          loginDetails.idSocket = socket.id
+          loginDetails.numberOfClients = players.length
 
-        // Printar o que foi recebido
-        console.log(data);
+          socket.emit('ROOM_CREATED', loginDetails , players)
+        } else if (players[1] == null) {
+          console.log(`O user ${loginDetails.name} entrou na sala ${loginDetails.roomId}, o server emitiu ROOM_JOINED`)
+          
+          players[1] = socket.id;
+          loginDetails.isCreator = false
+          loginDetails.idSocket = socket.id
+          loginDetails.numberOfClients = players.length
+
+          socket.emit('ROOM_JOINED', loginDetails, players)
+        }
+
+        console.log(players)
+        // TODO: IMPLEMENT THE OTHER USERS CASE
+
+        const roomClients = io.sockets.adapter.rooms[loginDetails.roomId] || { length: 0 }
+        /*
+        if (roomClients.length == 0) {
+            console.log(`Criando sala ${loginDetails.roomId}, o user ${loginDetails.name} emitiu room_created`)
+            loginDetails.isCreator = true 
+            loginDetails.idSocket = socket.id
+            loginDetails.numberOfClients = roomClients.length+1
+
+            //prepareRoom(socket,loginDetails)
+            socket.join(loginDetails.roomId)
+            
+            socket.emit('ROOM_CREATED', loginDetails)
+        } else {
+            console.log(`Entrou na sala ${loginDetails.roomId}, o user ${loginDetails.name} emitiu room_joined`)
+
+            console.log(`User ${loginDetails.name} has enter in the room ${loginDetails.roomId}`)
+            loginDetails.isCreator = false
+            loginDetails.idSocket = socket.id
+            loginDetails.numberOfClients = roomClients.length+1
+
+            //prepareRoom(socket,loginDetails)
+            socket.join(loginDetails.roomId)
+
+            socket.emit('ROOM_JOINED', loginDetails)
+        }
 
         var clientId = socket.id;
         console.log("Received User Join" + clientId);
         
         var actualClients = JSON.stringify(clients)
         clients.push(clientId)
-
-        // Respondendo ao usuario con a confirmação
-        socket.emit('ROOM_JOINED', {some: "data"});
+        */
     });
+
+    socket.on("OFFER", (socketId, description) => {
+      socket.to(socketId).emit("OFFER", socket.id, description);
+    });
+
+    socket.on("ANSWER", (socketId, description) => {
+      socket.to(socketId).emit("ANSWER", socket.id, description);
+    });
+
+    //TODO: Review this implementation
+    socket.on('ICE_CANDIDATE', (event) => {
+      socket.broadcast.to(event.roomId).emit('ICE_CANDIDATE', event)
+    })
+
+
 });
+
+async function prepareRoom(socket, data){
+    const roomId = data.roomId;
+    const existRoom = Room.getRoom(roomId);
+    if (existRoom) {
+      console.log('--- use exist room. roomId=' + roomId);
+    } else {
+      console.log('--- create new room. roomId=' + roomId);
+      const room = await setupRoom(roomId);
+    }
+  }
+
+  async function setupRoom(name) {
+    const room = new Room(name);
+    const router = await worker.createRouter();
+    router.roomname = name;
+  
+    router.observer.on('close', () => {
+      console.log('-- router closed. room=%s', name);
+    });
+    
+    router.observer.on('newtransport', transport => {
+      console.log('-- router newtransport. room=%s', name);
+    });
+  
+    room.router = router;
+    Room.addRoom(room, name);
+    return room;
+  }
+
+  class Room {
+    constructor(name) {
+      this.name = name;
+      this.producerTransports = {};
+      this.messageProducers = {};
+  
+      this.consumerTransports = {};
+      this.messageConsumerSets = {};
+  
+      this.router = null;
+    }
+  
+    getProducerTrasnport(id) {
+      console.log('ROOM getProducerTransport')
+      return this.producerTransports[id];
+    }
+  
+    addProducerTrasport(id, transport) {
+      this.producerTransports[id] = transport;
+      console.log('room=%s producerTransports count=%d', this.name, Object.keys(this.producerTransports).length);
+    }
+  
+    removeProducerTransport(id) {
+      delete this.producerTransports[id];
+      console.log('room=%s producerTransports count=%d', this.name, Object.keys(this.producerTransports).length);
+    }
+  
+    getProducer(id, label) {
+      if (label === 'chat') {
+        return this.messageProducers[id];
+      }else {
+        console.warn('ROOM UNKNOWN producer label=' + label);
+      }
+    }
+  
+    getRemoteIds(clientId, label) {
+      let remoteIds = [];
+      if (label === 'chat') {
+        for (const key in this.messageProducers) {
+          if (key !== clientId) {
+            remoteIds.push(key);
+          }
+        }
+      }
+      return remoteIds;
+    }
+  
+    addProducer(id, producer, label) {
+      if (label === 'chat') {
+        this.messageProducers[id] = producer;
+        console.log('room=%s messageProducers count=%d', this.name, Object.keys(this.messageProducers).length);
+      }else {
+        console.warn('ROOM addProducer UNKNOWN producer label=' + label);
+      }
+    }
+  
+    removeProducer(id, label) {
+      if (label === 'chat') {
+        delete this.messageProducers[id];
+        console.log('messageProducers count=' + Object.keys(this.messageProducers).length);
+      }else {
+        console.warn('ROOM UNKNOWN producer label=' + label);
+      }
+    }
+  
+    getConsumerTrasnport(id) {
+      return this.consumerTransports[id];
+    }
+  
+    addConsumerTrasport(id, transport) {
+      this.consumerTransports[id] = transport;
+      console.log('room=%s add consumerTransports count=%d', this.name, Object.keys(this.consumerTransports).length);
+    }
+  
+    removeConsumerTransport(id) {
+      delete this.consumerTransports[id];
+      console.log('room=%s remove consumerTransports count=%d', this.name, Object.keys(this.consumerTransports).length);
+    }
+  
+    getConsumerSet(localId, label) {
+      if (label === 'chat') {
+        return this.messageConsumerSets[localId];
+      }else {
+        console.warn('WARN: getConsumerSet() UNKNWON label=%s', label);
+      }
+    }
+  
+    addConsumerSet(localId, set, label) {
+      if (label === 'chat') {
+        this.messageConsumerSets[localId] = set;
+      } else {
+        console.warn('WARN: addConsumerSet() UNKNWON label=%s', label);
+      }
+    }
+  
+    removeConsumerSetDeep(localId) {
+      const messageSet = this.getConsumerSet(localId, 'chat');
+      delete this.messageConsumerSets[localId];
+      if (messageSet) {
+        for (const key in messageSet) {
+          const consumer = messageSet[key];
+          consumer.close();
+          delete messageSet[key];
+        }
+  
+        console.log('room=%s removeConsumerSetDeep message consumers count=%d', this.name, Object.keys(messageSet).length);
+      }
+    }
+  
+    getConsumer(localId, remoteId, label) {
+      const set = this.getConsumerSet(localId, label);
+      if (set) {
+        return set[remoteId];
+      }
+      else {
+        return null;
+      }
+    }
+  
+  
+    addConsumer(localId, remoteId, consumer, label) {
+      const set = this.getConsumerSet(localId, label);
+      if (set) {
+        set[remoteId] = consumer;
+        console.log('room=%s consumers label=%s count=%d', this.name, label, Object.keys(set).length);
+      }
+      else {
+        console.log('room=%s new set for label=%s, localId=%s', this.name, label, localId);
+        const newSet = {};
+        newSet[remoteId] = consumer;
+        this.addConsumerSet(localId, newSet, label);
+        console.log('room=%s consumers label=%s count=%d', this.name, label, Object.keys(newSet).length);
+      }
+    }
+  
+    removeConsumer(localId, remoteId, label) {
+      const set = this.getConsumerSet(localId, label);
+      if (set) {
+        delete set[remoteId];
+        console.log('room=%s consumers label=%s count=%d', this.name, label, Object.keys(set).length);
+      }
+      else {
+        console.log('NO set for room=%s label=%s, localId=%s', this.name, label, localId);
+      }
+    }
+  
+    // --- static methtod ---
+    static staticInit() {
+      rooms = {};
+    }
+  
+    static addRoom(room, name) {
+      Room.rooms[name] = room;
+      console.log('static addRoom. name=%s', room.name);
+      //console.log('static addRoom. name=%s, rooms:%O', room.name, room);
+    }
+  
+    static getRoom(name) {
+      return Room.rooms[name];
+    }
+  
+    static removeRoom(name) {
+      delete Room.rooms[name];
+    }
+  }
+
+  Room.rooms = {};
+
+  // --- default room ---
+let defaultRoom = null
 
 const port = 3000
 server.listen(port)
